3095e89f42f788465febba9fa3b80bbb35f52f14
pablocardozogodoi <namatacu5@gmail.com>
2024-06-17 20:53:59 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,8 +4,8 @@ function setup() {
   }
   
   function draw() {
-    stroke(" blue");
-    fill("red")
+    stroke("rgb(0,165,103)");
+    fill("rbg(95,2,155)");
   
     
     if (mouseIsPressed) {
