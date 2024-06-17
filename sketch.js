bd257e1c2380235de11f1e8ce6399e912a1866ee
pablocardozogodoi <namatacu5@gmail.com>
2024-06-17 20:57:17 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,14 @@
 function setup() {
-  createCanvas(400, 400);
-  background("#FFC107"); // Corrected background color format
-}
-
-function draw() {
-  stroke("rgb(0,165,103)"); // Set stroke color to RGB(0,165,103)
-  fill("rgb(95,2,155)"); // Set fill color to RGB(95,2,155)
-
-  if (mouseIsPressed) {
-    rect(mouseX, mouseY, 20, 35); // Draw a rectangle at the mouse position
+    createCanvas(400, 400);
+    background("#FF076E")
   }
-}
+  
+  function draw() {
+    stroke("rgb(0,165,103)");
+    fill("rgb(95,2,155)");
+    
+    
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 20, 35);
+    }
+  }
